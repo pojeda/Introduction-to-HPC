ce74151e39688f3233947c3280fbf1d6c90610a5
Pedro Ojeda-May <pedroojeda2011@googlemail.com>
2021-01-15 16:47:34 +0100

    R serial and arrays examples

---
+++ R/Job-Arrays/job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -A hpc2n2021-001
+#Asking for 10 min.
+#SBATCH -t 00:12:00
+#SBATCH -N 1
+##SBATCH -n 1
+#SBATCH --array=1-28
+##Writing the output and error files
+#SBATCH --output=Array_test.%A_%a.out
+#SBATCH --error=Array_test.%A_%a.error
+
+ml GCC/8.2.0-2.31.1  OpenMPI/3.1.3
+ml R/3.6.0
+
+R --no-save --no-restore -f script.R
+
